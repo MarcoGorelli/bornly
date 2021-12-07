1230e074db970615a0986d7af806804a764bff80
Marco Gorelli <marcogorelli@protonmail.com>
2021-12-07 20:54:05 +0000

    add testing notebook and colours

--- bornly.py
+++ bornly.py
@@ -4,56 +4,59 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 COLOR_PALETTE = [
-    (0.12156862745098039, 0.4666666666666667, 0.7058823529411765),
-    (1.0, 0.4980392156862745, 0.054901960784313725),
-    (0.17254901960784313, 0.6274509803921569, 0.17254901960784313),
-    (0.8392156862745098, 0.15294117647058825, 0.1568627450980392),
-    (0.5803921568627451, 0.403921568627451, 0.7411764705882353),
-    (0.5490196078431373, 0.33725490196078434, 0.29411764705882354),
-    (0.8901960784313725, 0.4666666666666667, 0.7607843137254902),
-    (0.4980392156862745, 0.4980392156862745, 0.4980392156862745),
-    (0.7372549019607844, 0.7411764705882353, 0.13333333333333333),
-    (0.09019607843137255, 0.7450980392156863, 0.8117647058823529),
+    (31.0, 119.0, 180.0),
+    (255.0, 127.0, 14.0),
+    (44.0, 160.0, 44.0),
+    (214.0, 39.0, 40.0),
+    (148.0, 103.0, 189.0),
+    (140.0, 86.0, 75.0),
+    (227.0, 119.0, 194.0),
+    (127.0, 127.0, 127.0),
+    (188.0, 189.0, 34.0),
+    (23.0, 190.0, 207.0),
 ]
 
+def get_colors(n, alpha):
+    return [f"rgba({color[0]}, {color[1]}, {color[2]}, {alpha})" for color in COLOR_PALETTE[:n]]
 
-def lineplot(data, x, y, hue=None, color_palette=None):
-    def plot_hue(df_, name, label, color):
-        rgb = f"rgba({color[0]*255}, {color[1]*255}, {color[2]*255}"
-        fill_rgba = f"{rgb},0)"
-        line_rgba = f"{rgb},1)"
-        fig = px.line(
-            df_,
-            x=x,
-            y="value",
-            color="variable",
-            color_discrete_map={f"{y}_lower": fill_rgba, f"{y}_upper": fill_rgba},
-        )
+def _plot_hue(df_, name, label, color, x, y):
+    rgb = f"rgba({color[0]}, {color[1]}, {color[2]}"
+    fill_rgba = f"{rgb},0)"
+    line_rgba = f"{rgb},1)"
+    fig = px.line(
+        df_,
+        x=x,
+        y="value",
+        color="variable",
+        color_discrete_map={f"{y}_lower": fill_rgba, f"{y}_upper": fill_rgba},
+    )
 
-        fig.update_traces(
-            name="interval", selector=dict(name=f"{y}_upper"), showlegend=False
-        )
-        fig.update_traces(fill="tonexty")
-        if name is None:
-            fig.update_traces(
-                fillcolor="rgba(0,0,0,0)",
-                selector=dict(name=y),
-                line_color=line_rgba,
-                showlegend=False,
-            )
-        else:
-            fig.update_traces(
-                fillcolor="rgba(0,0,0,0)",
-                selector=dict(name=name),
-                name=label,
-                line_color=line_rgba,
-            )
+    fig.update_traces(  # is this necessary? what's interval?
+        name="interval", selector=dict(name=f"{y}_upper"), showlegend=False
+    )
+    fig.update_traces(fill="tonexty")
+    if name is None:
         fig.update_traces(
             fillcolor="rgba(0,0,0,0)",
+            selector=dict(name=y),
+            line_color=line_rgba,
             showlegend=False,
-            selector=dict(name=f"{y}_lower"),
         )
-        return fig
+    else:
+        fig.update_traces(
+            fillcolor="rgba(0,0,0,0)",
+            selector=dict(name=name),
+            name=label,
+            line_color=line_rgba,
+        )
+    fig.update_traces(
+        fillcolor="rgba(0,0,0,0)",
+        showlegend=False,
+        selector=dict(name=f"{y}_lower"),
+    )
+    return fig
+
+def lineplot(data, x, y, hue=None, color_palette=None):
 
     if hue is None:
         group = [x]
@@ -78,9 +81,9 @@ def lineplot(data, x, y, hue=None, color_palette=None):
     if hue is not None:
         for hue_, color in zip(pdf[hue].unique(), color_palette):
             df_ = pdf[pdf[hue] == hue_].copy()
-            figs.append(plot_hue(df_, y, hue_, color))
+            figs.append(_plot_hue(df_, y, hue_, color, x, y))
     else:
-        figs.append(plot_hue(pdf, None, None, color_palette[0]))
+        figs.append(_plot_hue(pdf, None, None, color_palette[0], x, y))
 
     data = figs[0].data
     for fig in figs[1:]:
