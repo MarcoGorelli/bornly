6a358597b2613eea1312243d8a9e9f39a58a3fdd
Marco Gorelli <marcogorelli@protonmail.com>
2021-12-10 08:58:58 +0000

    wip - time to start day job

--- bornly/__init__.py
+++ bornly/__init__.py
@@ -1,12 +1,13 @@
 import functools
 
 import numpy as np
+from numpy.core.fromnumeric import var
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import scipy.stats as stats
 from plotly.subplots import make_subplots
-from bornly._seaborn.seaborn.relational import _ScatterPlotter
+import bornly._seaborn.seaborn as _sns
 
 COLOR_PALETTE = [
     (31.0, 119.0, 180.0),
@@ -67,6 +68,10 @@ class Ax:
         for key, val in kwargs.items():
             getattr(self, f"set_{key}")(val)
 
+    def fill_between(self, x, y1, y2, alpha=None, color=None, legend=None):
+        # need to figure this out if I want to make progress...
+        ...
+
 
 def _add_to_fig(subplot, figure, row, col):
     for data in subplot.data:
@@ -101,11 +106,13 @@ def subplots(nrows=1, ncols=1, *, sharex=False, sharey=False, **kwargs):
     return fig, np.asarray(ax)
 
 
-def get_colors(n, alpha):
+def get_colors(n, alpha, palette=None):
+    if palette is None:
+        palette = COLOR_PALETTE
     if n == -1:
-        colors = COLOR_PALETTE
+        colors = palette
     else:
-        colors = COLOR_PALETTE[:n]
+        colors = palette[:n]
     return [f"rgba({color[0]}, {color[1]}, {color[2]}, {alpha})" for color in colors]
 
 
@@ -233,7 +240,7 @@ def scatterplot1(data, x, y, hue=None, ax=None):
                 i.showlegend = False
         ax(figure)
 
-class ScatterPlotter(_ScatterPlotter):
+class ScatterPlotter(_sns.relational._ScatterPlotter):
 
     @property
     def reverse_variables(self):
@@ -252,12 +259,18 @@ class ScatterPlotter(_ScatterPlotter):
             'data_frame': data,
             'x': self.variables['x'],
             'y': self.variables['y'],
-            'color_discrete_sequence': get_colors(-1, 1),
         }
+        if 'palette' in self.variables:
+            plotting_kwargs['color_discrete_sequence'] = get_colors(-1, 1, _sns.color_palette(self.variables['palette']))
+        else:
+            plotting_kwargs['color_discrete_sequence'] = get_colors(-1, 1)
         if 'hue' in self.variables:
-            plotting_kwargs = {**plotting_kwargs, **{'color': self.variables['hue']}}
+            plotting_kwargs['color'] = self.variables['hue']
         if 'size' in self.variables:
-            plotting_kwargs = {**plotting_kwargs, **{'size': self.variables['size']}}
+            plotting_kwargs['size'] = self.variables['size']
+        if 'sizes' in self.variables:
+            plotting_kwargs['size_max'] = self.variables['sizes'][1]
+        
         fig = px.scatter(**plotting_kwargs)
         ax.set_xlabel(self.variables['x'])
         ax.set_ylabel(self.variables['y'])
@@ -290,6 +303,12 @@ def scatterplot(
         estimator=estimator, ci=ci, n_boot=n_boot,
         alpha=alpha, x_jitter=x_jitter, y_jitter=y_jitter, legend=legend,
     )
+    if palette is not None:
+        p.variables['palette'] = palette
+    if sizes is not None:
+        p.variables['sizes'] = sizes
+    if hue_order is not None:
+        raise NotImplementedError('hue_order isn\'t available yet')
 
     if ax is None:
         _, ax = subplots()
@@ -300,3 +319,154 @@ def scatterplot(
     p.plot(ax, kwargs)
     return ax._figure
 
+class LinePlotter(_sns.relational._LinePlotter)
+    def plot(self, ax, kws):
+        """Draw the plot onto an axes, passing matplotlib kwargs."""
+
+        # Draw a test plot, using the passed in kwargs. The goal here is to
+        # honor both (a) the current state of the plot cycler and (b) the
+        # specified kwargs on all the lines we will draw, overriding when
+        # relevant with the data semantics. Note that we won't cycle
+        # internally; in other words, if ``hue`` is not used, all elements will
+        # have the same color, but they will have the color that you would have
+        # gotten from the corresponding matplotlib function, and calling the
+        # function will advance the axes property cycle.
+
+        kws.setdefault("markeredgewidth", kws.pop("mew", .75))
+        kws.setdefault("markeredgecolor", kws.pop("mec", "w"))
+
+        # Set default error kwargs
+        err_kws = self.err_kws.copy()
+        if self.err_style == "band":
+            err_kws.setdefault("alpha", .2)
+        elif self.err_style == "bars":
+            pass
+        elif self.err_style is not None:
+            err = "`err_style` must be 'band' or 'bars', not {}"
+            raise ValueError(err.format(self.err_style))
+
+        # Initialize the aggregation object
+        agg = _sns.EstimateAggregator(
+            self.estimator, self.errorbar, n_boot=self.n_boot, seed=self.seed,
+        )
+
+        # TODO abstract variable to aggregate over here-ish. Better name?
+        agg_var = "y"
+        grouper = ["x"]
+
+        # TODO How to handle NA? We don't want NA to propagate through to the
+        # estimate/CI when some values are present, but we would also like
+        # matplotlib to show "gaps" in the line when all values are missing.
+        # This is straightforward absent aggregation, but complicated with it.
+        # If we want to use nas, we need to conditionalize dropna in iter_data.
+
+        # Loop over the semantic subsets and add to the plot
+        grouping_vars = "hue", "size", "style"
+        for sub_vars, sub_data in self.iter_data(grouping_vars, from_comp_data=True):
+
+            if self.sort:
+                sort_vars = ["units", "x", "y"]
+                sort_cols = [var for var in sort_vars if var in self.variables]
+                sub_data = sub_data.sort_values(sort_cols)
+
+            if self.estimator is not None:
+                if "units" in self.variables:
+                    # TODO eventually relax this constraint
+                    err = "estimator must be None when specifying units"
+                    raise ValueError(err)
+                grouped = sub_data.groupby(grouper, sort=self.sort)
+                # Could pass as_index=False instead of reset_index,
+                # but that fails on a corner case with older pandas.
+                sub_data = grouped.apply(agg, agg_var).reset_index()
+
+            # TODO this is pretty ad hoc ; see GH2409
+            for var in "xy":
+                if self._log_scaled(var):
+                    for col in sub_data.filter(regex=f"^{var}"):
+                        sub_data[col] = np.power(10, sub_data[col])
+
+            # --- Draw the main line(s)
+
+            if "units" in self.variables:   # XXX why not add to grouping variables?
+                lines = []
+                for _, unit_data in sub_data.groupby("units"):
+                    lines.extend(ax.plot(unit_data["x"], unit_data["y"], **kws))
+            else:
+                lines = ax.plot(sub_data["x"], sub_data["y"], **kws)
+
+            for line in lines:
+
+                if "hue" in sub_vars:
+                    line.set_color(self._hue_map(sub_vars["hue"]))
+
+                if "size" in sub_vars:
+                    line.set_linewidth(self._size_map(sub_vars["size"]))
+
+                if "style" in sub_vars:
+                    attributes = self._style_map(sub_vars["style"])
+                    if "dashes" in attributes:
+                        line.set_dashes(attributes["dashes"])
+                    if "marker" in attributes:
+                        line.set_marker(attributes["marker"])
+
+            line_color = line.get_color()
+            line_alpha = line.get_alpha()
+
+            # --- Draw the confidence intervals
+
+            if self.estimator is not None and self.errorbar is not None:
+
+                # TODO handling of orientation will need to happen here
+
+                if self.err_style == "band":
+
+                    ax.fill_between(
+                        sub_data["x"], sub_data["ymin"], sub_data["ymax"],
+                        color=line_color, **err_kws
+                    )
+
+                elif self.err_style == "bars":
+
+                    error_deltas = (
+                        sub_data["y"] - sub_data["ymin"],
+                        sub_data["ymax"] - sub_data["y"],
+                    )
+                    ebars = ax.errorbar(
+                        sub_data["x"], sub_data["y"], error_deltas,
+                        linestyle="", color=line_color, alpha=line_alpha,
+                        **err_kws
+                    )
+
+
+
+def lineplot(
+    *,
+    x=None, y=None,
+    hue=None, size=None, style=None,
+    data=None,
+    palette=None, hue_order=None, hue_norm=None,
+    sizes=None, size_order=None, size_norm=None,
+    dashes=True, markers=None, style_order=None,
+    units=None, estimator="mean", n_boot=1000, seed=None,
+    sort=True, err_style="band", err_kws=None,
+    legend="auto",
+    errorbar=("ci", 95),
+    ax=None, **kwargs
+):
+
+    variables = LinePlotter.get_semantics(locals())
+    p = LinePlotter(
+        data=data, variables=variables,
+        estimator=estimator, n_boot=n_boot, seed=seed,
+        sort=sort, err_style=err_style, err_kws=err_kws, legend=legend,
+        errorbar=errorbar,
+    )
+
+    if ax is None:
+        _, ax = subplots()
+
+    if not p.has_xy_data:
+        return ax._figure
+
+    p.plot(ax, kwargs)
+    return ax._figure
